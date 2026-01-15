fd99718ebf92be02a3c675c0c7f834c42f3ec712
Vakiyama <vakiyama@outlook.com>
2026-01-15 11:10:40 -0800

    improvement: reorder skills, add rust, update arkhet entry

--- flake.nix
+++ flake.nix
@@ -1,19 +1,25 @@
 {
   description = "node project flake";
-
+  #
+  #    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  #    nixpkgs-unstable2.url = "github:NixOS/nixpkgs/nixos-unstable";
+  #  };
+  #
+  #  outputs = { self, nixpkgs, nixpkgs-unstable, nixpkgs-unstable2 }:
+  #
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs-unstable }: {
 
     devShell.x86_64-linux =
       let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
       in
-      pkgs.mkShell rec {
+      pkgs.mkShell {
         buildInputs = with pkgs; [
-          bun
+          nodejs_24
         ];
 
         shellHook = ''
